09784e999af2f2082d58afa92c2cf7f4cdc8a2f7
s905301071 <s905301071@gmail.com>
2020-03-21 17:16:59 +1300

    Add project files.

---
+++ Assignment 1A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment 1A");
+        }
+    }
+}
